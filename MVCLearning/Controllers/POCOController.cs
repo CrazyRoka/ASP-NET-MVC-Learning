8d6954011765bd927614c5b12888945a8648c8bd
Rostyslav <RokaRostuk@gmail.com>
2019-03-23 00:22:05 +0200

    Created POCO Controller

---
+++ MVCLearning/Controllers/POCOController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCLearning.Controllers
+{
+    public class POCOController
+    {
+        public IActionResult Index() => "this is POCO controller";
+
+        [ActionContext]
+        public ActionContext ActionContext { get; set; }
+
+        public HttpContext HttpContext => ActionContext.HttpContext;
+
+        public ModelStateDictionary ModelState => ActionContext.ModelState;
+
+        public string UserAgentInfo()
+        {
+            if (HttpContext.Request.Headers.ContainsKey("User-Agent"))
+            {
+                return HttpContext.Request.Headers["User-Agent"];
+            }
+            return "No user-agent information";
+        }
+    }
+}
