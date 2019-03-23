7ecc62623deaea621914ec1939ccba4f3917a362
Rostyslav <RokaRostuk@gmail.com>
2019-03-23 12:20:55 +0200

    Added post requests

---
+++ MVCLearning/Controllers/SubmitDataController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCLearning.Models;
+
+namespace MVCLearning.Controllers
+{
+    public class SubmitDataController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult CreateMenu() => View();
+
+        [HttpPost]
+        public IActionResult CreateMenu(int id, string text, double price, DateTime date, string category)
+        {
+            var menu = new Menu { Id = id, Text = text, Price = price, Date = date, Category = category };
+            ViewBag.Info = $"menu created: {menu.Text}, Price: {menu.Price}, " +
+                $"date: {menu.Date.ToShortDateString()}, category: {menu.Category}";
+            return View("Index");
+        }
+
+        public IActionResult CreateMenu2() => View();
+
+        [HttpPost]
+        public IActionResult CreateMenu2(Menu menu)
+        {
+            ViewBag.Info = $"menu created: {menu.Text}, Price: {menu.Price}, " +
+                $"date: {menu.Date.ToShortDateString()}, category: {menu.Category}";
+            return View("Index");
+        }
+
+        public IActionResult CreateMenu3() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMenu3Result()
+        {
+            var menu = new Menu();
+            bool updated = await TryUpdateModelAsync<Menu>(menu);
+            if (updated)
+            {
+                ViewBag.Info = $"menu created: {menu.Text}, Price: {menu.Price}, " +
+                    $"date: {menu.Date.ToShortDateString()}, category: {menu.Category}";
+                return View("Index");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        public IActionResult CreateMenu4() => View();
+
+        [HttpPost]
+        public IActionResult CreateMenu4(Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Info = $"menu created: {menu.Text}, Price: {menu.Price}, " +
+                    $"date: {menu.Date.ToShortDateString()}, category: {menu.Category}";
+                return View("Index");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
--- MVCLearning/Models/Menu.cs
+++ MVCLearning/Models/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace MVCLearning.Models
     public class Menu
     {
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string Text { get; set; }
+        [Display(Name = "Price"), DisplayFormat(DataFormatString = "0:C")]
         public double Price { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+        [StringLength(10)]
         public string Category { get; set; }
     }
 }
